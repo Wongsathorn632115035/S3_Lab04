c71284cd9b1b8e06de1420bec4f252f2e8884cd8
markwst <wongsathorn_w@cmu.ac.th>
2022-07-31 17:30:18 +0700

    4.1

--- src/views/event/EventLayoutView.vue
+++ src/views/event/EventLayoutView.vue
@@ -33,6 +33,10 @@ export default {
       })
       .catch((error) => {
         console.log(error)
+        this.$router.push({
+          name: '404Resource',
+          params: { resource: 'event' }
+        })
       })
   }
 }
